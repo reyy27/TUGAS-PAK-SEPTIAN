fb1324ed1a8682652d2f691af1a0eadaf2d96186
reyy27 <113427200+reyy27@users.noreply.github.com>
2022-09-29 10:16:56 +0700

    Add files via upload

---
+++ TUGAS ALGO SESI 3/main(1).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+int menu;
+  cout <<" masukan nomor menu ";
+  cin >> menu;
+  if(menu==1){
+    cout <<"cake tape";
+  }else if(menu==2){
+    cout <<"bolu kukus";
+  }else if(menu==3){
+    cout <<"black forest";
+  }else
+    cout <<"kue pukis";
+      
+return 0;
+}
